9106abae951948292b194dfb4515edc1efc80d59
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-27 08:24:40 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,2 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.lagradost.cloudstream3.plugins") {
-                useModule("com.github.recloudstream:gradle:-SNAPSHOT")
-            }
-        }
-    }
-}
-
 rootProject.name = "ToonStreamExtension"
-include(":ToonStream")
+include("ToonStream")
